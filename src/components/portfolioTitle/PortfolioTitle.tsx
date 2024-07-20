888bb62ba92d8382583338d5b225ff9d5f2a6aa3
mrjakhongir <nusratovjahongir@gmail.com>
2024-07-20 19:10:17 +0500

    feat: create reusable components for portfolio.

---
+++ src/components/portfolioTitle/PortfolioTitle.tsx
@@ -0,0 +1,42 @@
+import Image from 'next/image';
+
+type PortfolioTitleProps = {
+	title: string;
+	icon: string;
+	logo: string;
+	subtitle: string;
+	bg: string;
+	color: string;
+	position: string;
+};
+
+function PortfolioTitle({
+	logo,
+	icon,
+	title,
+	subtitle,
+	bg,
+	color,
+	position,
+}: PortfolioTitleProps) {
+	return (
+		<div
+			className={`flex flex-col items-start ${
+				position === 'right' && 'laptop:translate-x-[53%]'
+			} gap-4 mb-5`}>
+			<Image
+				className='w-auto'
+				src={logo}
+				alt={title}
+				width={250}
+				height={50}
+			/>
+			<div className={`flex gap-2 bg-${bg} p-2 rounded-full `}>
+				<Image src={icon} alt={title} width={23} height={23} />
+				<span className={`text-${color} text-[14px]`}>{subtitle}</span>
+			</div>
+		</div>
+	);
+}
+
+export default PortfolioTitle;
--- src/components/text/Text.tsx
+++ src/components/text/Text.tsx
@@ -6,7 +6,7 @@ type TextProps = {
 function Text({ content, color, marginBottom }: TextProps) {
 	return (
 		<p
-			className={`text-${color} text-[16px] laptop:text-[24px] leading-[30px] laptop:leading-[40px] text-center laptop:text-left font-monrope font-medium mb-${marginBottom} max-w-[70%] laptop:max-w-full`}>
+			className={`text-${color} text-[18px] laptop:text-[24px] leading-[30px] laptop:leading-[40px] text-center laptop:text-left font-monrope font-medium mb-${marginBottom} max-w-[70%] laptop:max-w-full`}>
 			{content}
 		</p>
 	);
