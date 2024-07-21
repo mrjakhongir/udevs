2e3e7bd117ec946a44845c343bc63a8b95a0c3a5
mrjakhongir <nusratovjahongir@gmail.com>
2024-07-21 23:26:44 +0500

    feat: add typewriter to the welcome section.

--- src/layouts/welcome/Welcome.tsx
+++ src/layouts/welcome/Welcome.tsx
@@ -1,6 +1,18 @@
+'use client';
 import Image from 'next/image';
+import { Cursor, useTypewriter } from 'react-simple-typewriter';
 
 function Welcome() {
+	const [text] = useTypewriter({
+		words: [
+			'User interface, User experience design',
+			'Optimization IT consulting infrastructure',
+			'IT consulting',
+			'Development of mobile applications',
+			'Development and implementation ERP systems',
+		],
+		loop: Infinity,
+	});
 	return (
 		<section className="bg-[url('/assets/pictures/section-bg.png')] bg-no-repeat bg-cover bg-center px-4">
 			<div className='w-full max-w-[1170px] mx-auto py-20 laptop:py-24 flex flex-col laptop:flex-row items-center gap-12'>
@@ -12,8 +24,13 @@ function Welcome() {
 						width={300}
 						height={41}
 					/>
-					<h1 className='text-2xl laptop:text-[40px] laptop:leading-[54px] font-semibold mb-5 text-secondary'>
-						IT-Outsourcing Company
+					<h1 className='text-2xl laptop:text-[40px] laptop:leading-[54px] font-semibold mb-5'>
+						<span className='text-secondary'>IT-Outsourcing Company</span>
+						<br />
+						<span className='text-primary'>
+							{text}
+							<Cursor />
+						</span>
 					</h1>
 					<a
 						className='bg-[#1b5bf7] py-4 px-20 text-white text-xl font-semibold rounded-md cursor-pointer transition-all hover:scale-105 select-none hidden laptop:block'
